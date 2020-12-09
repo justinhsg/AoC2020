538cd9f3e06dffe49a4f41a4b302f72d0464c3ca
Justin <justinhsg@gmail.com>
2020-12-09 10:40:49 +0000

    day 9

---
+++ 9/solution.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import re
+from collections import deque
+
+with open(os.path.join(sys.path[0], 'input' if len(sys.argv)==1 else 'sample'), "r") as infile:
+    numbers = list(map(int, infile.read().split("\n")))
+    preamble = 25 if len(sys.argv)==1 else 5
+    
+part1 = None
+part2 = None
+
+sums = dict()
+
+for i,n_i in enumerate(numbers[:preamble]):
+    
+    for j, n_j in enumerate(numbers[i+1:preamble]):
+        if(n_i+n_j not in sums):
+            sums[n_i+n_j] = 0
+        sums[n_i+n_j]+=1
+
+for k in range(preamble, len(numbers)):
+    n_k = numbers[k]
+    
+    if(n_k not in sums):
+        part1 = n_k
+        break
+    n_r = numbers[k-preamble]
+    
+    for i in range(k-preamble+1, k):
+        n_a = numbers[i]
+        
+        if(n_a + n_k not in sums):
+            sums[n_a+n_k] = 0
+        sums[n_a+n_k] += 1
+        sums[n_a+n_r] -= 1
+        if(sums[n_a+n_r]==0):
+            del sums[n_a+n_r]
+    
+
+start = 0
+end = 1
+cur_sum = numbers[start]
+while(start != len(numbers)):
+    #print(cur_sum, numbers[start:end])
+    if(cur_sum < part1 and end==len(numbers)):
+        part2 = None
+        break
+    elif(cur_sum < part1):
+        cur_sum += numbers[end]
+        end += 1
+    elif(cur_sum > part1):
+        cur_sum -= numbers[start]
+        start += 1
+    elif(start-end == 1):
+        start += 1
+    else:
+        subarr = numbers[start:end]
+        part2 = min(subarr)+max(subarr)
+        break
+
+print(part1)
+print(part2)
+    
